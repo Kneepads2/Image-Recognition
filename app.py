92eb083931a2cff0337ce4550a5a6b94c8436e0f
Kneepads2 <dylant6649@gmail.com>
2025-05-02 18:55:57 -0400

    making stuff

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
+transform = transforms.Compose(
+    {transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))}
+)
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
+
